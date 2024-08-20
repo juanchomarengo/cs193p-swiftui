6036c0e53fedfc8f9aa03010727e98cb8d1cff32
juanchomarengo <juanmmarengo@gmail.com>
2024-08-19 21:11:19 -0300

    Initial Commit

---
+++ Memorize-cs193p/Memorize_cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_cs193pApp.swift
+//  Memorize-cs193p
+//
+//  Created by Juancho Marengo on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
